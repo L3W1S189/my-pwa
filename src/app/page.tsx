087688c6df442fa14b7227d980df4f84f2bc1dde
Lewis Sebrell <lewissebrell@Lewiss-MacBook-Pro.local>
2026-01-02 21:39:19 -0600

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">My First iPhone Web App 🚀</h1>
+    <main className="min-h-screen bg-black text-white flex items-center justify-center p-8">
+      <h1 className="text-4xl font-bold">Lewis’ App ✅</h1>
     </main>
   );
 }
